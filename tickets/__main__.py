00ae070002b31694233f3b1b5e48ac717fc30c35
Chinmaya Panda <chinmaya@almug.ai>
2020-12-23 17:29:46 +0530

    added docs for main module

--- tickets/__main__.py
+++ tickets/__main__.py
@@ -1,15 +1,29 @@
 import fileinput
+from typing import List, Iterator
 
 from . import cmds, models, views
 
 
-def process(lot, line):
+def process(lot: models.ParkingLot, line: str) -> str:
+    """Executes the command on the parking lot instance `lot`.
+    Takes the output of the `model_func`, calls the `view_func` 
+    and returns the string.
+
+    Any errors during the processing are raised.
+    """
     cmd = cmds.parse(line)
     r = getattr(lot, cmd['model_func'])(*cmd['model_args'])
     return getattr(views, cmd['view_func'])(r)
 
 
-def process_lines(lines):
+def process_lines(lines: List[str]) -> Iterator[str]:
+    """Iterates through the lines, processes each and yields the output
+    or a rendered error line.
+
+    The first line is processed differently from the rest.
+    This is to ensure that a parking lot has been created
+    before other operarions are possible.
+    """
     lines = iter(lines)
 
     # the first line must be to create_parking_lot
@@ -36,6 +50,9 @@ def process_lines(lines):
 
 
 def main():
+    """Can take input from multiple sources from the command line,
+    e.g. file paths, redirects, pipes and stdin.
+    """
     for output in process_lines(fileinput.input()):
         print(output)
 
