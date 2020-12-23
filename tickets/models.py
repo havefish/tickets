610fb5d0c09116e4eb1b7e16333c4572eb67bd8f
Chinmaya Panda <chinmaya@almug.ai>
2020-12-23 17:20:49 +0530

    added documentation for models

--- tickets/models.py
+++ tickets/models.py
@@ -1,10 +1,29 @@
+"""Defines the core data structures and objects.
+
+Two entities are defined
+1. Car: a struct containsing the following fields
+
+    i. id: str -- registration number of the car
+    ii. age: int -- age of the driver
+    iii. slot: int -- alloted slot
+
+2. ParkingLot: a class the objects of which use the Car objects
+
+"""
 import heapq
 from collections import defaultdict, namedtuple
+from typing import List, Optional
 
 Car = namedtuple('Car', ['id', 'age', 'slot'])
 
 class ParkingLot:
-    def __init__(self, capacity):
+    """The objects of this class makes use of the Car objects and provides
+    the necessary ticketing operations.
+
+    i. includes a min-heap that stores the available slots at a given time
+    ii. indices: for fast aggregations based on age and car registration number.
+    """
+    def __init__(self, capacity: int):
         try:
             self.capacity = int(capacity)
             assert self.capacity > 0
@@ -22,7 +41,16 @@ class ParkingLot:
         self.cars = dict()
         self.ages = defaultdict(set)
                 
-    def park(self, id, age):
+    def park(self, id: str, age: int) -> Car:
+        """Allots a slot for a car with registration number `id` 
+        and `age` of the driver.
+
+        If the same car is given multiple times, it stores only once.
+        The subsequent calls return the previosly created Car object.
+
+        Raises:
+            Exception: if no slots are available.
+        """
         if id in self.cars:
             return self.slots[self.cars[id]]
         
@@ -40,7 +68,12 @@ class ParkingLot:
         
         return car
     
-    def leave(self, slot):
+    def leave(self, slot: int) -> Car:
+        """Empties the given slot and returns the Car object.
+
+        Raises:
+            Exception: if the slot is empty.
+        """
         car = self.slots[slot]
         
         if car is None:
@@ -54,11 +87,18 @@ class ParkingLot:
         heapq.heappush(self.available, slot)
         return car
         
-    def find_cars_for_age(self, age):
+    def find_cars_for_age(self, age: int) -> List[Car]:
+        """Returns the list of cars whole drivers are of the given age.
+        """
         return [self.slots[slot] for slot in self.ages.get(age, [])]
     
-    def find_slots_for_age(self, age):
+    def find_slots_for_age(self, age: int) -> List[int]:
+        """Returns the list of slots occupied by cars whose drivers are
+        of the given age.
+        """
         return list(self.ages.get(age, []))
-    
-    def find_slot_for_car(self, id):
+
+    def find_slot_for_car(self, id: str) -> Optional[int]:
+        """Retuns the slot occupied by the given car registration number
+        """
         return self.cars.get(id)
